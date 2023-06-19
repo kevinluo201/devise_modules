91bf0d71e7d482a49b274f5ffbe6850eb8046c60
Kevin Luo <kevin.luo@telus.com>
2023-06-18 22:26:24 -0400

    enable :database_authenticatable

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_18_222054) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_19_014104) do
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
