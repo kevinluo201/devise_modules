a2fc6f1188fc1e0bd834278ec2d4d488fdd1b5e5
Kevin Luo <kevin.luo@telus.com>
2023-06-20 15:50:58 -0400

    add recoverable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :confirmable, :validatable
+  devise :database_authenticatable, :registerable, :confirmable, :validatable,
+         :recoverable
 end
